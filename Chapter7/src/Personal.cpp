ec13b7bc810dca3dc26c23450de78e8bc3d3138f
Th2mas <tkhlebovitch@hotmail.com>
2023-02-20 17:11:53 +0100

    Use constructor delegation

--- Chapter7/src/Personal.cpp
+++ Chapter7/src/Personal.cpp
@@ -4,16 +4,20 @@
 #include "../include/Personal.h"
 
 // We use the scope operator ::
-Personal::Personal(): name(""), personalNumber(0), salary(1800) {
+Personal::Personal() : Personal("", 0, 1800) {
 }
 
-Personal::Personal(std::string name): name(name), personalNumber(0), salary(0) {
+// Without the element initializer, 'name' would first be an empty string (every string is first initialized with "")
+// and only then we would pass the actual param -> more efficient
+// Only with an element initializer it is possible to "instantiate" a const variable
+Personal::Personal(std::string name) : Personal(name, 0, 0) {
 }
 
-Personal::Personal(std::string name, unsigned int personalNumber): name(name), personalNumber(personalNumber), salary(0) {
+Personal::Personal(std::string name, unsigned int personalNumber) : Personal(name, personalNumber, 0) {
 }
 
-Personal::Personal(std::string name, unsigned int personalNumber, unsigned int salary): name(name), personalNumber(personalNumber), salary(salary) {
+Personal::Personal(std::string name, unsigned int personalNumber, unsigned int salary) :
+        name(name), personalNumber(personalNumber), salary(salary) {
 }
 
 const std::string &Personal::getName() const {
